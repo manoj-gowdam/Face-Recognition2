826e52b10e56f050f38086123c812853aa23013e
1ms17me105 <1ms17me105@gmail.com>
2023-12-21 19:44:45 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# pip install -r requirements.txt
+# import mediapipe as mp
+# import cv2
+
+# mp_face_detection = mp.solutions.face_detection
+# facedetection = mp_face_detection.FaceDetection(min_detection_confidence=0.4)
+
+# img = cv2.imread("pics/nihal/me.jpg")
+
+# predictions = facedetection.process(img)
+
+# print(predictions[0])
+from deepface import DeepFace
